5bf0b141effba6a7de3c4a3518bd0f1539d13cb3
lemazo <delahais.b@gmail.com>
2016-10-19 16:27:14 +0200

    Add 4 classes : Data, TimeSeries, Datum and Sensor

--- src/model/index.js
+++ src/model/index.js
@@ -1,2 +1,34 @@
 
-/* TODO : Créer le modèle objet ici */ 
+/* TODO : Créer le modèle objet ici */
+
+class Data {}
+
+class TimeSeries extends Data {
+  constructor(id, name, values, labels) {
+    super(id, name);
+    this.values = values;
+    this.labels = labels;
+  }
+}
+
+class Datum extends Data {
+  constructor(id, name, value) {
+    super(id, name);
+    this.value = value;
+  }
+}
+
+class Sensor {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  get id() {
+    return this.id;
+  }
+
+  get name() {
+    return this.name;
+  }
+}
